63046fd9354c9de62f4f4886f2a7a617024d6ae4
Chidera Munachimso <okechukwuchidera12@gmail.com>
2023-03-30 06:46:04 +0100

    added prettier config and modified overlay in JS

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const burgerMenu = document.querySelector(".menu");
-const navbar = document.querySelector("ul.nav-menu");
-const closeNav = document.querySelector(".close");
+'use strict';
+const burgerMenu = document.querySelector('.menu');
+const navbar = document.querySelector('ul.nav-menu');
+const closeNav = document.querySelector('.close');
+const overlay = document.querySelector('.overlay');
 
-burgerMenu.addEventListener("click", function () {
-  navbar.style.display = "block";
+burgerMenu.addEventListener('click', function () {
+  navbar.style.display = 'block';
+  overlay.classList.remove('hidden');
 });
 
-closeNav.addEventListener("click", function () {
-  navbar.style.display = "none";
+closeNav.addEventListener('click', function () {
+  navbar.style.display = 'none';
+  overlay.classList.add('hidden');
 });
