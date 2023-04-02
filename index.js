79e167235f9905277e31ff4c25cd87695ba3f43a
Chidera Munachimso <okechukwuchidera12@gmail.com>
2023-04-02 14:09:42 +0100

    added functionality to the next and previous btn

--- index.js
+++ index.js
@@ -13,3 +13,24 @@ closeNav.addEventListener('click', function () {
   navbar.style.display = 'none';
   overlay.classList.add('hidden');
 });
+
+//changing slides
+const nextBtn = document.querySelector('.next-icon');
+const prevBtn = document.querySelector('.prev-icon');
+const sneakerSlide = document.querySelectorAll('.sneakers');
+let currentSlide = 0;
+
+const showSlide = function () {
+  sneakerSlide.forEach(sneaker => sneaker.classList.remove('active'));
+  sneakerSlide[currentSlide].classList.add('active');
+};
+const nextSneaker = function () {
+  currentSlide = (currentSlide + 1) % sneakerSlide.length;
+  showSlide();
+};
+const prevSneaker = function () {
+  currentSlide = (currentSlide - 1 + sneakerSlide.length) % sneakerSlide.length;
+  showSlide();
+};
+nextBtn.addEventListener('click', nextSneaker);
+prevBtn.addEventListener('click', prevSneaker);
